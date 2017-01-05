3ea239a720d4b38bcae8a3a6e8121e835b88ebd8
Franz Brauße <brausse@uni-trier.de>
2017-01-05 11:41:00 +0100

    sqrt(REAL): documentation reformat

--- src/sqrt.cc
+++ src/sqrt.cc
@@ -76,53 +76,57 @@ REAL sqrt(const REAL & x) { return limit(sqrt_approx, x); }
 /*!
  * \brief Computes the square root of the argument \a x.
  *
- * For domain \f$[A;B]\f$ with \f$A>0\f$, the square root function
- * \f$f=\sqrt{}\f$ has a Lipschitz bound:
- * \f[ \forall x,y\in[A;B]:
- *     |f(x)-f(y)| \leq K\cdot|x-y| = K\cdot|f(x)+f(y)|\cdot|f(x)-f(y)|
- *     \Longleftrightarrow 1/|f(x)+f(y)| \leq K
- * \f]
- * Since \f$\max_{x,y \in [A;B]} 1/|f(x)+f(y)| = 1/|2f(A)|\leq K\f$ for the
- * function's argument \f$x\in[x_c\pm x_\varepsilon]\f$ the error
- * therefore is
- * \f$|f(x_c)-f(x)|
- * \leq K|x-x_c| < Kx_\varepsilon
- * \leq x_\varepsilon/(\sqrt{\check x}+\sqrt{\check x-x_\varepsilon})
- * <    x_\varepsilon/(2\sqrt{\check x-x_\varepsilon})\f$.
+ * - For domain \f$[A;B]\f$ with \f$A>0\f$, the square root function
+ *   \f$f=\sqrt{}\f$ has a Lipschitz bound:
+ *   \f[ \forall x,y\in[A;B]:
+ *       |f(x)-f(y)| \leq K\cdot|x-y| = K\cdot|f(x)+f(y)|\cdot|f(x)-f(y)|
+ *       \Longleftrightarrow 1/|f(x)+f(y)| \leq K
+ *   \f]
+ *   Since \f$\max_{x,y \in [A;B]} 1/|f(x)+f(y)| = 1/|2f(A)|\leq K\f$ for the
+ *   function's argument \f$x\in[x_c\pm x_\varepsilon]\f$ the error
+ *   therefore is
+ *   \f$|f(x_c)-f(x)|
+ *   \leq K|x-x_c| < Kx_\varepsilon
+ *   \leq x_\varepsilon/(\sqrt{\check x}+\sqrt{\check x-x_\varepsilon})
+ *   <    x_\varepsilon/(2\sqrt{\check x-x_\varepsilon})\f$.
  *
- * This case, \f$x\geq A=\check x-x_\varepsilon>0\f$, is chosen when
- * \f$\check x\geq 4x_\varepsilon\Longrightarrow |x|\geq 3x_\varepsilon\f$,
- * therefore \f$|\sqrt{x_c}-\sqrt x|
- * \leq x_\varepsilon/(2\sqrt{\check x-x_\varepsilon})\f$.
- * The absolute precision used to compute
- * \f$\sqrt{x_c}\f$ is \f$\max\{P,e[z]\}\f$ where
- * \f$z\geq x_\varepsilon/(2\sqrt{\check x-x_\varepsilon})\f$ and
- * \f$e[v]=e\f$ for any \ref sizetype representation \f$m\cdot2^e\f$ of \f$v\f$.
- * The relative precision is \f$\max\{e[\check x]+P,e[z]\}\f$.
+ *   This case, \f$x\geq A=\check x-x_\varepsilon>0\f$, is chosen when
+ *   \f$\check x\geq 4x_\varepsilon\Longrightarrow |x|\geq 3x_\varepsilon\f$,
+ *   therefore \f$|\sqrt{x_c}-\sqrt x|
+ *   \leq x_\varepsilon/(2\sqrt{\check x-x_\varepsilon})\f$.
+ *   The absolute precision used to compute
+ *   \f$\sqrt{x_c}\f$ is \f$\max\{P,e[z]\}\f$ where
+ *   \f$z\geq x_\varepsilon/(2\sqrt{\check x-x_\varepsilon})\f$ and
+ *   \f$e[v]=e\f$ for any \ref sizetype representation \f$m\cdot2^e\f$ of
+ *   \f$v\f$. The relative precision is \f$\max\{e[\check x]+P,e[z]\}\f$.
  *
- * Otherwise, if \f$x\neq0\f$,
- * \f$4x_\varepsilon\geq\check x=\hat x-2^e>\hat x\cdot(1-2^{-32})\f$,
- * therefore \f$\sqrt{|x|}
- * <\sqrt{\hat x+x_\varepsilon}
- * <\sqrt{4x_\varepsilon/(1-2^{-32})+x_\varepsilon}
- * <2.25\sqrt{x_\varepsilon}\f$
- * and \f$0\approx\sqrt{|x|}\f$ is chosen as an approximation with error
- * \f$\geq3\sqrt{x_\varepsilon}\f$.
- *
- * \todo
- * For \f$0\in[x_c\pm x_\varepsilon]\f$ use
- * \f$\approx[\sqrt{x_\varepsilon}\pm\sqrt{x_\varepsilon}]\f$ as result to decrease
- * the error and disable representation of non-co-domain values smaller than
- * zero.
+ * - Otherwise, \f$x=0\f$ or
+ *   \f$4x_\varepsilon\geq\check x=\hat x-2^e>\hat x\cdot(1-2^{-32})\f$,
+ *   then \f$\sqrt{|x|}
+ *   <\sqrt{\hat x+x_\varepsilon}
+ *   <\sqrt{4x_\varepsilon/(1-2^{-32})+x_\varepsilon}
+ *   <2.25\sqrt{x_\varepsilon}\f$
+ *   and \f$0\approx\sqrt{|x|}\f$ is chosen as an approximation with error
+ *   \f$\geq3\sqrt{x_\varepsilon}\f$.
  *
  * \todo
- * To determine \f$0\notin[x_c\pm x_\varepsilon]\f$ check
- * \f$\check x\geq cx_\varepsilon\f$ with \f$c\in\{2,3\}\f$ for tighter error
- * bounds in both cases.
+ * For \f$0\in[x_c\pm x_\varepsilon]\f$ use \f$
+ *    \approx[\sqrt{x_\varepsilon}\pm\sqrt{x_\varepsilon}]
+ * \f$
+ * as result to decrease the error and disable representation of non-co-domain
+ * values smaller than zero.
+ *
+ * \todo
+ * To determine \f$0\notin[x_c\pm x_\varepsilon]\f$ check \f$
+ *    \check x\geq cx_\varepsilon\f$ with \f$c\in\{2,3\}
+ * \f$
+ * for tighter error bounds in both cases.
  *
  * \todo
  * In case \f$0\notin[x_c\pm x_\varepsilon]\f$, does it make sense to use the
- * bound \f$x_\varepsilon/(\sqrt{\check x}+\sqrt{\check x-x_\varepsilon})\f$
+ * bound \f$
+ *    x_\varepsilon/(\sqrt{\check x}+\sqrt{\check x-x_\varepsilon})
+ * \f$
  * instead of \f$x_\varepsilon/(2\sqrt{\check x-x_\varepsilon})\f$?
  *
  * \param x non-negative real number
