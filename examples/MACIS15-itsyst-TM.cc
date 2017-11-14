88b00fceaa39a39b5081e79a37a29c52f1824e0b
Franz Brauße <brausse@informatik.uni-trier.de>
2017-11-14 11:01:29 +0100

    examples/MACIS15-itsyst-TM.cc: cleanup indentation

--- examples/MACIS15-itsyst-TM.cc
+++ examples/MACIS15-itsyst-TM.cc
@@ -1,27 +1,26 @@
-#include "iRRAM.h"
+
 #include "TaylorModel.h"
+#include <iRRAM.h>
+
 using namespace iRRAM;
 
-void compute(){
+void compute()
+{
+	REAL c;
+	cin >> c;
+	int n;
+	cin >> n;
 
-  REAL c;
-  cin >> c;
-  int n;
-  cin >> n;
+	TM x = REAL(0.125);
 
-  TM   x = REAL(0.125 );
+	for (int i = 0; i <= n; i++) {
 
-  
-  for( int i=0; i<=n; i++){
-    
-    cout << x<< "\n";
-    TM::polish(x);
-    
-    cout << x<< "\n";
-    cout << REAL(x) << "\n";
+		cout << x << "\n";
+		TM::polish(x);
 
-    x=x*c*(REAL(1)-x);
+		cout << x << "\n";
+		cout << REAL(x) << "\n";
 
-  }
+		x = x * c / (REAL(1) - x);
+	}
 }
-
